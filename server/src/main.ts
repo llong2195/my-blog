b2503a18fa091e3e9281affc14964a5caefd9621
llong2195 <nduylong9501@gmail.com>
2022-12-24 23:12:39 +0700

    refactor

---
+++ server/src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({ origin: '*' });
+
+  await app.listen(PORT, () => console.log(`Server running om port: ${PORT}`));
+}
+bootstrap();
