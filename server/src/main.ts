8cf029093eb269f7890710ea1faef031c8511d9d
llong2195 <nduylong9501@gmail.com>
2022-12-25 11:17:46 +0700

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,9 @@ async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors();
+  app.enableCors({ origin: '*' , credentials: true});
 
   await app.listen(PORT, () => console.log(`Server running om port: ${PORT}`));
-
 }
 
 bootstrap();
