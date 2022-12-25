c97d698eed8301ff6d71c512dfa0771eca397bdc
llong2195 <nduylong9501@gmail.com>
2022-12-25 11:20:46 +0700

    fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.use(cookieParser());
-  app.enableCors({ origin: '*' , credentials: true});
+  if(configService.get("NODE_ENV") == "development"){
+    app.enableCors();
+  }else{
+    app.enableCors({ origin: '*' , credentials: true});
+  }
 
   await app.listen(PORT, () => console.log(`Server running om port: ${PORT}`));
 }
